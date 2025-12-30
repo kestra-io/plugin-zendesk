f78fbe65471382677763c2b41231d6ecdf7e3600
ben8t <pimpaudben@gmail.com>
2025-12-30 15:43:42 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/zendesk/package-info.java
+++ src/main/java/io/kestra/plugin/zendesk/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using a Zendesk API.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = PluginSubGroup.PluginCategory.OTHER,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.zendesk;
 
--- src/main/java/io/kestra/plugin/zendesk/tickets/package-info.java
+++ src/main/java/io/kestra/plugin/zendesk/tickets/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "Zendesk Tickets",
     description = "This sub-group of plugins contains tasks for managing Zendesk tickets.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = PluginSubGroup.PluginCategory.OTHER,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.zendesk.tickets;
 
